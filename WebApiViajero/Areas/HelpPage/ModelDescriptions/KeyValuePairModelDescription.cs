9cf01e049a676782c22c25ebf301173862d7049c
Elias Silva <silvaelias@hotmail.com>
2019-09-12 14:06:21 -0400

    Eliminada carpeta WebApiViajero

--- WebApiViajero/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApiViajero.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
